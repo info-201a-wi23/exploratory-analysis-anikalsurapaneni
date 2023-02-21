89ec6caf18fd72d9ed6cc895933708b980f1d03e
anjals5 <anjals5@uw.edu>
2023-02-21 14:06:16 -0800

    updated chart 1 with new link and edits

--- chart_1.R
+++ chart_1.R
@@ -4,6 +4,7 @@ library("dplyr")
 library("stringr")
 library("ggplot2")
 
+install.packages("XQuartz")
 
 whs_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-anikalsurapaneni/main/finalwhc.csv", stringsAsFactors = FALSE)
 View(whs_df)
@@ -14,13 +15,14 @@ danger_per_date <- whs_df %>%
   summarize(total_danger = sum(danger))
   View(danger_per_date)
 
-ggplot( data = danger_per_date) +
+ ggplot(data = danger_per_date) +
   geom_col(aes(x = date_inscribed,
                y = total_danger)) +
   labs(title = "Number of Endangered Sites Based
      on Their Year of Inscription", 
        x = "Date of Inscription", 
        y = "Number of Heritage Sites Endangered")
+
 
 
 
