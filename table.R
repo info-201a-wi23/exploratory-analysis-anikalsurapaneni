ecf98eb801f94f24e2014a26db632f405f6913f7
arizo32 <arizo32@uw.edu>
2023-02-20 22:57:54 -0800

    Table

--- table.R
+++ table.R
@@ -1,1 +1,13 @@
-# table
+library(dplyr)
+library(stringr)
+
+whc_df <- read.csv("whc-sites-2021.csv", stringsAsFactors = FALSE)
+View(whc_df)
+
+
+whc_sub <- whc_df %>%
+  select("Name", "date_inscribed", "danger", "category_long") %>%
+  arrange(desc(date_inscribed))
+
+View(head(whc_sub, 10))
+
