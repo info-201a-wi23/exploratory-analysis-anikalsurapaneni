a4bc7a4943536b101c251c846d1a81797cd1bd54
arizo32 <arizo32@uw.edu>
2023-02-21 22:22:30 -0800

    table and summary

--- sum_info.R
+++ sum_info.R
@@ -1,1 +1,6 @@
 # sum info
+summary_info <- list()
+summary_info$num_observations <- nrow(my_dataframe)
+summary_info$some_max_value <- my_dataframe %>%
+  filter(some_var == max(some_var, na.rm = T)) %>%
+  select(some_label)
--- table.R
+++ table.R
@@ -1,13 +1,16 @@
 library(dplyr)
 library(stringr)
 
-whc_df <- read.csv("whc-sites-2021.csv", stringsAsFactors = FALSE)
+whc_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-anikalsurapaneni/main/finalwhc.csv", stringsAsFactors = FALSE)
 View(whc_df)
 
 
 whc_sub <- whc_df %>%
   select("Name", "date_inscribed", "danger", "category_long") %>%
-  arrange(desc(date_inscribed))
+  arrange(desc(danger))
 
-View(head(whc_sub, 10))
+whc_sub_10 <- whc_sub %>% slice(1:10)
+
+View(whc_sub_10)
+
 
